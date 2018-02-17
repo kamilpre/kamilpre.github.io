38b7d2079a479933414f7cdac3e07614c46bc7a1
Kamil Predel <kamil.predel@gmail.com>
2018-02-17 16:52:05 +0100

    ustaw-tlo-warsztaty

---
+++ warsztaty/ustaw-tlo/js/js.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function ustawTlo() {
+   
+    var allParagraf = document.getElementsByTagName('p');
+    
+    
+    
+    allParagraf[0].style.backgroundColor = 'red';
+    allParagraf[1].style.backgroundColor = 'yellow';
+}
+
+//var input = document.getElementById('input-id');
