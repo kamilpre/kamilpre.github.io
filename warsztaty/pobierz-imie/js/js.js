aba2618e4aaad56a4d81fb3ce9e43f861abdf8a6
Kamil Predel <kamil.predel@gmail.com>
2018-02-17 17:01:04 +0100

    pobierz imie warsztat

---
+++ warsztaty/pobierz-imie/js/js.js
@@ -0,0 +1,9 @@
+'use strict';
+function pobierzImie(event) {
+    
+    event.preventDefault();
+//    console.log('działa');
+    var form = document.getElementById('formularz');
+//    console.log('działa');
+    
+}
