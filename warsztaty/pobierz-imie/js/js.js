ad4a4514ed5b85361a5b35760d8ee6f451773716
Kamil Predel <kamil.predel@gmail.com>
2018-02-18 09:38:50 +0100

    pobierz imie formularz js

--- warsztaty/pobierz-imie/js/js.js
+++ warsztaty/pobierz-imie/js/js.js
@@ -5,5 +5,10 @@ function pobierzImie(event) {
 //    console.log('działa');
     var form = document.getElementById('formularz');
 //    console.log('działa');
-    
+    for (var i = 0; i < form.length; i++) {
+//        console.log(form.elements[i].value);
+        if (form.elements[i].value !='Submit') {
+//            console.log(form.elements[i].value);
+        }
+    }
 }
