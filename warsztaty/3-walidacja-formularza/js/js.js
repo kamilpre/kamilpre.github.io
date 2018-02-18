59b2064b8b6ecaaa2454197efbf7cf7e580ab737
Kamil Predel <kamil.predel@gmail.com>
2018-02-18 16:58:42 +0100

    walicja pustych pól

--- warsztaty/3-walidacja-formularza/js/js.js
+++ warsztaty/3-walidacja-formularza/js/js.js
@@ -21,9 +21,39 @@ var wiadomosc = document.getElementById('wiadomosc');
 function stanCheckboxa() {
     zgodaMarketingowa1.checked = this.checked;
     zgodaMarketingowa2.checked = this.checked;
-    
+
     zgodaMarketingowa1.disabled = this.checked;
     zgodaMarketingowa2.disabled = this.checked;
 }
 
 wszystkieZgody.onchange = stanCheckboxa;
+
+// funkcja walidujaca inputy
+
+
+function walidujFormularz(event) {
+
+    if (inputImie.value == '') {
+        //          console.log('dziala');
+        var msgImie = document.createElement('li');
+        msgImie.innerHTML = 'wpisz imię!';
+        wiadomosc.appendChild(msgImie);
+        event.preventDefault();
+    }
+    if (inputEmail.value == '') {
+        var msgEmail = document.createElement('li');
+        msgEmail.innerHTML = 'wpisz email!';
+        wiadomosc.appendChild(msgEmail);
+        event.preventDefault();
+    }
+    if (!zgodaMarketingowa1.checked) {
+        var msgZgoda = document.createElement('li');
+        msgZgoda.innerHTML = 'musisz wyracic zgode!';
+        wiadomosc.appendChild(msgZgoda);
+        event.preventDefault();
+}
+}
+
+
+    wyslijBtn.addEventListener('click', walidujFormularz); // pierwszy sposob
+    //wyslijBtn.onclick = walidujFormularz; // drugi sposob
